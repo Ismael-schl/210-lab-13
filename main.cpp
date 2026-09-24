a249f7af59582c3fcaff01b1775f616425f6eae5
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-23 18:09:16 -0700

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+const int SIZE = 150;
+
 struct sData {
     int sID;
     double eScore;
@@ -12,19 +14,29 @@ struct sData {
 
 void sortArray(sData* array);
 
+void outputArray(sData* array);
+
 void consoleUpdate(sData* array);
 
 int main() {
+    sData 
+    ifstream fin("210-lab-13-grades.txt")
+
 
 }
 
+void sortArray(sData* array) {
+    
 
+}
 
 
 void consoleUpdate(sData* array) {
-    cout << "Read " << sizeof(array) << " student records" << endl;
+    cout << "Read " << SIZE << " student records" << endl;
     cout << "Sorted results written to 210-lab-13-grades-sorted.txt" << endl;
     cout << endl << "--- Summary Statistics ---" << endl;
-    cout << "Minimum Score: " << << endl;
-    cout <<" Maximum Score: "
-}
+    cout << "Minimum Score: " << << " (Student ID: "<< << ")" << endl;
+    cout << "Maximum Score: " << << " (Student ID: "<< << ")" << endl;
+    cout << "Mean Score: " << << endl;
+    cout << "Median Score: " << << " (Student ID: "<< << ")" << endl;
+} 
